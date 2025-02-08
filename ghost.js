134df50893527a4ff0d1ca6391ac42a7c04c9ccf
Imrexxx-OG <imrexxx93@gmail.com>
2025-02-08 22:14:39 +0100

    make it better

--- ghost.js
+++ ghost.js
@@ -1,6 +1,10 @@
-const navBar = document.getElementById('nav-bar');
-const exploreBtn = document.getElementById('explore-btn');
+// const navBar = document.getElementById('nav-bar');
+// const exploreBtn = document.getElementById('explore-btn');
 
-exploreBtn.addEventListener("click", function() {
-  navBar.classList.toggle('hide-nav')
+// exploreBtn.addEventListener("click", function() {
+//   navBar.classList.toggle('hide-nav')
+// })
+
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  document.getElementById("nav-bar").classList.toggle("active")
 })
